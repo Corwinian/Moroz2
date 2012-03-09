d31d9eb807ae7da6e4883d58c85a2d14c56c367a
Corwin <DartRavern@gmail.com>
2012-03-09 21:28:42 +1100

    испрвил несколько мелких ошибок вхафмане

--- hafman.py
+++ hafman.py
@@ -4,13 +4,15 @@ import sys
 import heapq
 
 
+BITS_IN_BYTE=8
+
 class Hafman(object):
 	"""Hafman archivator"""
 
 	class Tree(object):
 		"""Tree node"""
 		def __init__(self, weight, ch=None, left=None, right=None):
-			super(Tree, self).__init__()
+			#super(Tree, self).__init__()
 			self.ch = ch
 			self.left = left
 			self.right = right
@@ -33,42 +35,43 @@ class Hafman(object):
 		self.bytes = byteArray
 
 	def create_freq_table(self):
-		for byte in bytes:
-			if byte in raw_table:
-				freq_table[byte] += 1
+		for byte in self.bytes:
+			if byte in self.freq_table:
+				self.freq_table[byte] += 1
 			else:
-				freq_table[byte] = 1
+				self.freq_table[byte] = 1
 
 	def make_tree(self):
-		tree = [ Tree(v,k) for k, v in raw_table.items()]
+		tree = [ self.Tree(v,k) for k, v in self.freq_table.items()]
 		heapq.heapify(tree)
 		while len(tree) > 1:
 			left, right = heapq.heappop(tree), heapq.heappop(tree)
-			parent = Tree(left.weight  + right.weight, left=left, right = right)
+			parent = self.Tree(left.weight  + right.weight, left=left, right = right)
 			heapq.heappush(tree, parent)
 
 		def convert(tree, l):
 			if tree.hameChildren():
-				convert(tree.left, l.append(False))
-				convert(tree.right, l.append(True))
+				convert(tree.left, l+ [False])
+				convert(tree.right, l+[True])
 			else:
-				hafman_table[tree.ch] = l
-
-		convert(tree[0], [])
+				self.hafman_table[tree.ch] = l
+		a = []
+		convert(tree[0], a)
 
 	def encode_freq_table(self):
 		res = bytearray([])
-		for byte in freq_table.keys:
+		for byte in self.freq_table.keys():
 			res.append(byte)
-			count = freq_table[byte]
+			count = self.freq_table[byte]
 			res.append(count//2**BITS_IN_BYTE)
 			res.append(count%2**BITS_IN_BYTE)
 		return res
 
 	def	encode_file(self):
-		BITS_IN_BYTE=8
+		bit_res = []
+		for byte in self.bytes:
+			bit_res.extend(self.hafman_table[byte])
 
-		bit_res = [self.hafman_table[byte] for byte in self.bytes]
 		step = 0
 		temp = 0
 		res = bytearray ([])
@@ -89,10 +92,10 @@ class Hafman(object):
 		return res
 
 	def encode(self):
-		create_freq_tabl()
-		make_tree()
+		self.create_freq_table()
+		self.make_tree()
 		res = bytearray([len(self.freq_table)])
-		res.extend(encode_freq_table())
-		res.extend(encode_file())
+		res.extend(self.encode_freq_table())
+		res.extend(self.encode_file())
 		return res
 
