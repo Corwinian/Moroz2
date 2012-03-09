2c2d30152403cf4389f878732e9c6aa769dbcb57
Corwin <DartRavern@gmail.com>
2012-03-09 18:57:30 +1100

    сделал заготовку для управляющей функции

---
+++ parser.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+
+def main():
+	return 0
+
+if __name__="main"
+	try:
+		sys.exit(main())
+	except KeyboardInterrupt:
+		console.writeline('inerrupted by user')
+	sys.exit(1)
