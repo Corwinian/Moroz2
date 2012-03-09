ceddece9ce1bb9987f5200ac755e7ed407a8de94
Corwin <DartRavern@gmail.com>
2012-03-09 19:39:49 +1100

    сделал разбор параметров командной строки

--- parser.py
+++ parser.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
 import os
+import sys
+import optparse
+
+def parse_options():
+	parser = optparse.OptionParser(usage='archivaor.py [options]')
+	parser.add_option('-a', '--archive', dest='archive', help='compress metod', metavar='FILE')
+	parser.add_option('-f', '--file', dest='file', help='road to file to compress', metavar='FILE')
+	parser.add_option('-o', '--output', dest='output', help='road to output file', metavar='FILE')
+	return parser.parse_args()
 
 def main():
+	(options, args ) = parse_options()
 	return 0
 
-if __name__="main"
+if __name__=="__main__":
 	try:
 		sys.exit(main())
 	except KeyboardInterrupt:
