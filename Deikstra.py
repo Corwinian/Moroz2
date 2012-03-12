20896ef4a35c94158c546cf495a631903e20724d
Corwin <DartRavern@gmail.com>
2012-03-12 15:50:40 +1100

    сделал вроде как нармальное преобразование к опз

--- Deikstra.py
+++ Deikstra.py
@@ -1,47 +1,67 @@
-#!/usr/bib/env python3
-op = {'+':1,'-':1,'*'2:,'/':2,'^':3}
+#!/usr/bin/env python3
+import sys
+op = {'+':1,'-':1,'*':2,'/':2,'^':3}
 
 def conver_to_tokens(str):
-  res =[]
-  buf=""
-  for ch in str:
-    if ch is not num and buf !="":
-      res.append(buf)
-      buf=""
-    if ch in '()' or ch in op.keys():
-      res.append(ch)
-    elif ch is int:
-      buf+=ch
-  return res
+	is_num = lambda x: x in '0123456789'
+	res =[]
+	buf=""
+	for ch in str:
+		if not is_num(ch) and buf !="":
+			res.append(int(buf))
+			buf=""
+		if ch in '()' or ch in op.keys():
+			res.append(ch)
+		elif is_num(ch):
+			buf+=ch
+			print("ch - {0}".format(ch))
+			print("buf - {0}".format(buf))
+
+	if buf != "":
+		res.append(int(buf))
+	return res
 
 def to_opz(posl):
-  res=[]
-  ops=[]
-  for ch in posl:
-    if ch is num:
-      res.append(ch)
-    elif ch in op.keys():
-      t = op.pop()
-      while(op[ch]<=op[t]):
-        res.apend(t)
-        t=op.pop()
-      ops.append(ch)
-    elif ch == '(':
-       ops.append(ch)
-    elif ch == ')':
-      t = ops.pop()
-      while t != '(':
-        res.append(t)
-        t = ops.pop()
-  return res
+	res=[]
+	ops=[]
+	for ch in posl:
+		if ch in op.keys():
+			print ('ch - {0}'.format(ch))
+			while(len(ops) > 0 and op[ch]<=op[ops[-1]]):
+				t=ops.pop()
+				res.apend(t)
+			ops.append(ch)
+		elif ch == '(':
+			ops.append(ch)
+		elif ch == ')':
+			t = ops.pop()
+			while t != '(':
+				res.append(t)
+				t = ops.pop()
+		else:
+			res.append(ch)
+	if len(ops):
+		while (len(ops)):
+			res.append(ops.pop())
+	return res
 
 def calc(opz):
 	while len(opz)!= 1:
 		op =opz.pop()
 		a = opz.pop()
 		b = opz.pop()
-		opz.append(eval("{0}{1}{2}".format(a,op,b)
+		opz.append(eval("{0}{1}{2}".format(a,op,b)))
 	return opz.pop()
 
-if __name__=="__main__"
+def main(argv):
+	print("expression - {0}".format(argv[1]))
+	tokens = conver_to_tokens(argv[1])
+	print("tokens - {0}".format(tokens))
+	opz = to_opz(tokens)
+	print("opz - {0}".format(opz))
+	res = calc(opz)
+	print("res - {0}".format(res))
 
+
+if __name__=="__main__":
+	main(sys.argv)
