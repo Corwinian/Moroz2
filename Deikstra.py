8cb7c7494ce4d38e3898cfd1716af5a7185878e4
Corwin <DartRavern@gmail.com>
2012-03-12 20:56:27 +1100

    сделал для целых чисел

--- Deikstra.py
+++ Deikstra.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 import sys
-op = {'+':1,'-':1,'*':2,'/':2,'^':3}
+op = {'+':1,'-':1,'*':2,'/':2,'^':3, '(':0, ')':0}
 
 def conver_to_tokens(str):
 	is_num = lambda x: x in '0123456789'
-	res =[]
+	res=[]
 	buf=""
 	for ch in str:
 		if not is_num(ch) and buf !="":
 			res.append(int(buf))
-			buf=""
+			buf= ""
 		if ch in '()' or ch in op.keys():
 			res.append(ch)
 		elif is_num(ch):
 			buf+=ch
-			print("ch - {0}".format(ch))
-			print("buf - {0}".format(buf))
 
 	if buf != "":
 		res.append(int(buf))
@@ -25,19 +23,16 @@ def to_opz(posl):
 	res=[]
 	ops=[]
 	for ch in posl:
-		if ch in op.keys():
-			print ('ch - {0}'.format(ch))
+		if ch in op.keys() and ch not in '()':
 			while(len(ops) > 0 and op[ch]<=op[ops[-1]]):
-				t=ops.pop()
-				res.apend(t)
+				res.append(ops.pop())
 			ops.append(ch)
 		elif ch == '(':
 			ops.append(ch)
 		elif ch == ')':
-			t = ops.pop()
-			while t != '(':
-				res.append(t)
-				t = ops.pop()
+			while ops[-1] != '(':
+				res.append(ops.pop())
+			ops.pop()
 		else:
 			res.append(ch)
 	if len(ops):
@@ -46,17 +41,11 @@ def to_opz(posl):
 	return res
 
 def calc(opz):
+	check_pow = lambda x: x if x != '^' else '**'
 	st = []
 	while len(opz) != 0:
 		if opz[0] in op.keys():
-			a = st.pop()
-			b = st.pop()
-			print("a - {0}".format(a))
-			print("b - {0}".format(b))
-			t =eval("{0}{1}{2}".format(b, opz.pop(0), a))
-			print("t - {0}".format(t))
-			opz.insert(0, t)
-			print(opz)
+			opz.insert(0, eval("{2}{1}{0}".format(st.pop(), check_pow(opz.pop(0)), st.pop())))
 		else:
 			st.append(opz.pop(0))
 	return st.pop()
